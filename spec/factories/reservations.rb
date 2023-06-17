c888f214459cb5f67b469dcc46bba3662df0d264
Dragan Fabijan <dragan.fabijan@gmail.com>
2023-06-17 23:11:19 +1000

    Add FactoryBot gem (#4)
    * Add FactoryBot gem and factories for Reservationa and Guest
    
    * Update guest specs and use built in email REGEXP
    
    * Cleanup

---
+++ spec/factories/reservations.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :reservation do
+    association    :guest
+    code           { "YYY12345678" }
+    start_date     { "2023-06-17" }
+    end_date       { "2023-06-18" }
+    currency       { "AUD" }
+    status         { "accepted" }
+    nights         { 1 }
+    payout_price   { "900.99" }
+    security_price { "100.99" }
+    total_price    { "1000.99" }
+    total_number_of_guests { 4 }
+    number_of_adults       { 2 }
+    number_of_children     { 1 }
+    number_of_infants      { 1 }
+  end
+end
