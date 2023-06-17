af42db4bf35ea41afc8f0270b4cc76c9a651f8f5
Dragan Fabijan <dragan.fabijan@gmail.com>
2023-06-17 15:24:48 +1000

    Add specs for a Guest model

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,5 +1,26 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Guest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Guest.new(email: "test@example.com", first_name: "Test", last_name: "Guest", phone_numbers: ["1234567890"]) }
+
+  describe "#validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:phone_numbers) }
+    it { should validate_uniqueness_of(:email) }
+
+    context "when email is invalid" do
+      it do
+        subject.email = "invalid email"
+        expect(subject).to_not be_valid
+        expect(subject.errors[:email]).to include("is invalid")
+      end
+    end
+
+    context "when email is valid" do
+      subject.email = "valid@example.com"
+      expect(subject).to be_valid
+    end
+  end
 end
