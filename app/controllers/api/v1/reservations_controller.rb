6a77b578498d0f7618dcf51dbc4b13f90aca18b4
Dragan Fabijan <dragan.fabijan@gmail.com>
2023-06-18 15:39:36 +1000

    Add comments

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class ReservationsController < ApplicationController
 
       def create
+        # TODO: Add services that will take care of reservation params
       end
 
     end
--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe "Api::V1::Reservations", type: :request do
   describe "POST /create" do
     it "returns http success" do
-      get "/api/v1/reservations"
-      expect(response).to have_http_status(:success)
+      # post "/api/v1/reservations"
+      # TODO: Test positive and negative scenarios
+      # expect(response).to have_http_status(:success)
     end
   end
 end
